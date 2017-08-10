c023d1aadc69da9c5e6842b70a7fcc50e1e83962
Daniel Bachhuber <daniel@handbuilt.co>
2017-08-10 12:38:53 -0700

    Rename base plugin file to match repo name

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/wp-unit-test-reporter.php';
+	require dirname( dirname( __FILE__ ) ) . '/phpunit-test-reporter.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
